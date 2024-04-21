c91d50d16792d4e1a0bc7107410666b86d1e2317
BERADQ <adqber123@outlook.com>
2024-04-21 20:57:53 +0800

    restart

--- kokoro-core/tests/tests.rs
+++
@@ -1,9 +0,0 @@
-use std::sync::Arc;
-
-use kokoro_core::context::{scope::Scope, Context};
-struct App;
-#[test]
-fn create_context() {
-    let app = App.into();
-    let _ctx = Context::create(Scope::create(Arc::clone(&app)).into(), app);
-}
