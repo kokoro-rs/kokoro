1729372333ecb82fbab29bdfa52e815740c9b0b0
BERADQ <adqber123@outlook.com>
2024-05-09 21:40:55 +0800

    啊啊，可恶的段错误

--- examples/dynplugin/src/lib.rs
+++ examples/dynplugin/src/lib.rs
@@ -1,17 +1,33 @@
 use std::sync::Arc;
 
 use kokoro_neo::plugin::Plugin;
-use kokoro_neo::result::*;
+use kokoro_neo::{export_plugin, plugin::dynamic::*, result::*};
 
 struct MyPlugin;
 
 impl Plugin<&'static str> for MyPlugin {
     fn load(ctx: kokoro_neo::context::Context<Self, &'static str>) -> Result<()> {
-        ctx.avails().add(|s: Arc<&str>| {
-            println!("{}", s);
-        });
+        //ctx.avails().add(print);
         Ok(())
     }
 }
+fn print(s: Arc<&str>) {
+    println!("from plugin {}", s);
+}
+impl Default for MyPlugin {
+    fn default() -> Self {
+        MyPlugin
+    }
+}
 
-extern "Rust" fn __load__() -> Result<()> {}
+export_plugin!(MyPlugin, MyPlugin::default(), &'static str);
+
+#[test]
+fn plugin() {
+    let ctx: kokoro_neo::context::Context<(), &'static str> = kokoro_neo::context::Context::new(());
+    ctx.avails().add(|s: Arc<&str>| {
+        println!("from super {}", s);
+    });
+    ctx.dyn_plug("dynplugin").unwrap();
+    ctx("bye");
+}
